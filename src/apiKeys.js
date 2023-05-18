8f5e20d6c896dce80b2408ec3686ffa4b592a747
Vikas Bhatt <79741490+vikibhatt@users.noreply.github.com>
2023-05-18 17:08:52 +0530

    Delete apiKeys.js

--- src/apiKeys.js
+++
@@ -1,5 +0,0 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
-module.exports = {
-  key: "86054c6df8a19509280ab2e6b92db9dd",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
